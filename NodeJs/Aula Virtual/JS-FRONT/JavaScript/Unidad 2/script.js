804616c8f6e24549f4851b9b4e9e024ba8eb4554
Edu Moreno <e.m.morenolp@gmail.com>
2024-04-30 22:37:21 -0300

    punto 4 ejercicio 15

--- NodeJs/Aula Virtual/JS-FRONT/JavaScript/Unidad 2/script.js
+++ NodeJs/Aula Virtual/JS-FRONT/JavaScript/Unidad 2/script.js
@@ -5,7 +5,7 @@ curso Full Stack”
 */
 
 function saludar(numero) {
-    for (let i = 0; i  < numero; ++i) {
+    for (let i = 0; i < numero; ++i) {
         console.log("Bienvenidos al curso Full Stack")
     }
 }
@@ -17,25 +17,25 @@ saludar(4)
 calcule el máximo.
 */
 
-function numeroMax(a,b) {
+function numeroMax(a, b) {
     if (a > b) {
         console.log("el numero mas grande es : " + a)
-    }else {
+    } else {
         console.log("el numero mas grande es : " + b)
     }
 }
-numeroMax(3,4)
+numeroMax(3, 4)
 
 /*
 3.Crear una función que se llame promedio3, que reciba como
 parámetro tres valores y devuelva el promedio de los mismos.
 */
 
-function promedio3(a,b,c) {
-    let resultado = (a + b + c)/3
+function promedio3(a, b, c) {
+    let resultado = (a + b + c) / 3
     console.log("Resultado promedio : " + resultado)
 }
-promedio3(2,6,4)
+promedio3(2, 6, 4)
 
 /*
 4.Crear una función que lea notas hasta que ingrese -1 y devuelve el
@@ -43,5 +43,32 @@ promedio de las notas leídas. ( aunque no se suele leer valores en una
 función)Una función que se llame siguiente, que reciba como
 parámetro un valor entero, y devuelva el siguiente del número
 ingresado como parámetro
-*/ 
+*/
 
+function calcularPromedioNotas() {
+    let suma = 0;
+    let cantidad = 0;
+    let nota = 0;
+    // Iterar hasta que se ingrese -1
+    do {
+        nota = parseInt(prompt("Ingrese una nota (-1 para terminar):"));
+        if (nota !== -1) {
+            suma += nota;
+            cantidad++;
+        }
+    } while (nota !== -1);
+    // Calcular promedio
+    const promedio = cantidad !== 0 ? suma / cantidad : 0;
+    return promedio;
+}
+
+const promedioNotas = calcularPromedioNotas();
+console.log("El promedio de las notas ingresadas es:", promedioNotas);
+
+function siguiente(numero) {
+    return numero + 1;
+}
+
+const numeroIngresado = 5;
+const siguienteNumero = siguiente(numeroIngresado);
+console.log("El siguiente número después de", numeroIngresado, "es:", siguienteNumero);
